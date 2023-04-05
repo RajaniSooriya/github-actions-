e467e7c05cee88e3a27d90945557aced0b76d204
Rajani Sooriyaarachchi <95573031+RajaniSooriya@users.noreply.github.com>
2023-04-06 02:31:56 +0530

    unit tests

---
+++ unit_test.py
@@ -0,0 +1,26 @@
+import os
+import tempfile
+from ppextractmodule import process
+
+def test_process_function():
+    # create a temporary directory to store extracted images
+    with tempfile.TemporaryDirectory() as tmpdir:
+        # create a test PowerPoint presentation file with three slides, each with one image
+        test_file = os.path.join(tmpdir, "test.pptx")
+        prs = Presentation()
+        for i in range(3):
+            slide = prs.slides.add_slide(prs.slide_layouts[0])
+            slide.shapes.add_picture("test_image.jpg", 0, 0)
+        prs.save(test_file)
+
+        # run the process function on the test file
+        process(test_file)
+
+        # check that three image files were extracted
+        assert len(os.listdir(tmpdir)) == 3
+
+        # check that each extracted image file exists and has a non-zero size
+        for i in range(3):
+            image_file = os.path.join(tmpdir, f"image{i}.jpg")
+            assert os.path.isfile(image_file)
+            assert os.path.getsize(image_file) > 0
